612e342d6b0c55171c58556699efae5bf1e7f915
MatheusSC017 <matheussimao2101@gmail.com>
2025-01-15 08:15:05 -0300

    Including UI to personal screen

--- app/src/main/java/com/example/academy/ui/workout/WorkoutFragment.java
+++ app/src/main/java/com/example/academy/ui/workout/WorkoutFragment.java
@@ -19,16 +19,11 @@ import com.example.academy.MainActivity;
 import com.example.academy.R;
 import com.example.academy.ui.base.JsonFragment;
 
-import java.util.AbstractMap;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 
