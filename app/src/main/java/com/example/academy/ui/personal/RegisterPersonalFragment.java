1cca154b7345e9563e6231c6d23ccf20d1874830
MatheusSC017 <matheussimao2101@gmail.com>
2025-01-17 07:18:51 -0300

    Including fragment to register personal data

---
+++ app/src/main/java/com/example/academy/ui/personal/RegisterPersonalFragment.java
@@ -0,0 +1,22 @@
+package com.example.academy.ui.personal;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.academy.R;
+
+public class RegisterPersonalFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register_personal, container, false);
+
+        return view;
+    }
+}
