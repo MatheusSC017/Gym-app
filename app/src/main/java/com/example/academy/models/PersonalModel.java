cbe2a8c6c3ea968d317f0631c291b27c0473392a
MatheusSC017 <matheussimao2101@gmail.com>
2025-02-15 10:22:04 -0300

    Including models to manage the personal data

--- app/src/main/java/com/example/academy/models/PersonalModel.java
+++ app/src/main/java/com/example/academy/models/PersonalModel.java
@@ -1,4 +1,77 @@
 package com.example.academy.models;
 
 public class PersonalModel {
+    private Long id;
+    private Float weight;
+    private Float height;
+    private Float leanMass;
+    private Float fatWeight;
+    private Float fatPercentage;
+    private Float imc;
+
+    public PersonalModel(Long id, Float weight, Float height, Float leanMass, Float fatWeight, Float fatPercentage, Float imc) {
+        setId(id);
+        setWeight(weight);
+        setHeight(height);
+        setLeanMass(leanMass);
+        setFatWeight(fatWeight);
+        setFatPercentage(fatPercentage);
+        setImc(imc);
+    }
+
+    public Float getFatPercentage() {
+        return fatPercentage;
+    }
+
+    public void setFatPercentage(Float fatPercentage) {
+        this.fatPercentage = fatPercentage;
+    }
+
+    public Float getFatWeight() {
+        return fatWeight;
+    }
+
+    public void setFatWeight(Float fatWeight) {
+        this.fatWeight = fatWeight;
+    }
+
+    public Float getHeight() {
+        return height;
+    }
+
+    public void setHeight(Float height) {
+        this.height = height;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Float getImc() {
+        return imc;
+    }
+
+    public void setImc(Float imc) {
+        this.imc = imc;
+    }
+
+    public Float getLeanMass() {
+        return leanMass;
+    }
+
+    public void setLeanMass(Float leanMass) {
+        this.leanMass = leanMass;
+    }
+
+    public Float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
 }
